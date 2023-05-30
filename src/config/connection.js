02beb7be8630bbe37fd588600843de3715600758
Angel Sanabria <angeleliusanabria@hotmail.com>
2023-05-30 10:54:38 -0500

    push

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 
 module.exports = () => {
-    return mysql.createConnection({
+    return mysql.createPool({
         host: 'byoqitgxwidg9ulnoywg-mysql.services.clever-cloud.com',
         user: 'usm9oaspwebg88ty',
         password: 'oRyowLtxxxq0QHY5CfJC',
         database: 'byoqitgxwidg9ulnoywg',
         port: '3306',
+        connectionLimit: 5
     })
 };
